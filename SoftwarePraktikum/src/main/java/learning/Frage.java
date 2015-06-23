c7abc787b26ea50614e9694cfbfc98d2c2f890de
lenamaier <lenamai.er@web.de>
2015-06-23 11:12:53 +0200

    Methoden in MC-Klasse verändert

--- SoftwarePraktikum/src/main/java/learning/Frage.java
+++ SoftwarePraktikum/src/main/java/learning/Frage.java
@@ -3,60 +3,11 @@ package learning;
 import java.util.ArrayList;
 
 public class Frage {
-	public int punktzahl; 
+	public static final int punktzahl = 3; 
 	public Benutzer ersteller;
 	public ArrayList<Medium> anhang; 
 	public int id;
 	public String titel; 
 	public String text;
 	
-	
-	public int getPunktzahl() {
-		return punktzahl;
-	}
-	
-	public void setPunktzahl(int punktzahl) {
-		this.punktzahl = punktzahl;
-	}
-	
-	public Benutzer getErsteller() {
-		return ersteller;
-	}
-	
-	public void setErsteller(Benutzer ersteller) {
-		this.ersteller = ersteller;
-	}
-	
-	public ArrayList<Medium> getAnhang() {
-		return anhang;
-	}
-	
-	public void setAnhang(ArrayList<Medium> anhang) {
-		this.anhang = anhang;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public String getTitel() {
-		return titel;
-	}
-	
-	public void setTitel(String titel) {
-		this.titel = titel;
-	}
-	
-	public String getText() {
-		return text;
-	}
-	
-	public void setText(String text) {
-		this.text = text;
-	} 
-
 }
--- SoftwarePraktikum/src/main/java/learning/MCFrage.java
+++ SoftwarePraktikum/src/main/java/learning/MCFrage.java
@@ -4,26 +4,39 @@ import java.util.ArrayList;
 
 public class MCFrage extends Frage {
 public ArrayList<String> antworten; 
-public int lösung; 
+public int loesung; 
+
+public MCFrage(Benutzer ersteller, String titel, String text, int loesung){
+	this.ersteller = ersteller;
+	this.titel = titel; 
+	this.text = text; 
+	this.loesung = loesung;
+	
+	antworten = new ArrayList<String>();
+	anhang = new ArrayList<Medium>();
+	
+}
 
 
 public ArrayList<String> getAntworten(){
 	return antworten;
 }
 
-public void setAntworten(ArrayList<String> antworten){
-	this.antworten = antworten;
+public void addAntworten(String antwort){
+	antworten.add(antwort);
 }
 
 public int getLösung(){
-	return lösung;
+	return loesung;
 }
 
-public void setLösung(int lösung){
-	this.lösung = lösung;
-}
 
 public int korrigiere(int antwort){
-	return antwort;
+	if (antwort == this.loesung){
+		return punktzahl;
+	}
+	else {
+		return 0;
+	}
 }
 }
