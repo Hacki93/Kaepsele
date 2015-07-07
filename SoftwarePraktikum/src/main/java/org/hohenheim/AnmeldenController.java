5b1873849647f847f6b0324e69124939008539b1
christoph94 <christoph.jachmann@gmail.com>
2015-07-07 13:47:48 +0200

    Seiten hinzugefügt
    Startseite und Anmeldeseite

--- SoftwarePraktikum/src/main/java/org/hohenheim/AnmeldenController.java
+++ SoftwarePraktikum/src/main/java/org/hohenheim/AnmeldenController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/angemeldet")
-public class GreetingController {
+@RequestMapping("/")
+public class AnmeldenController{
 	@RequestMapping(method=RequestMethod.GET)
 	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
-		return "index";
+		return "Anmelden";
 	}
 }
--- SoftwarePraktikum/src/main/java/org/hohenheim/EinstellungenController.java
+++ SoftwarePraktikum/src/main/java/org/hohenheim/EinstellungenController.java
@@ -7,11 +7,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/angemeldet")
-public class GreetingController {
+@RequestMapping("/einstellungen")
+public class EinstellungenController{
 	@RequestMapping(method=RequestMethod.GET)
 	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
-		return "index";
+		return "Einstellungen";
 	}
-}
+}	
+	
