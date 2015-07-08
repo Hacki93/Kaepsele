89cab7ccb3bbd21cc6eedb5707958a3dc39ce1f9
lenamaier <lenamai.er@web.de>
2015-07-08 15:34:16 +0200

    MVC Test

--- SoftwarePraktikum/src/main/java/controller/GreetingController.java
+++ SoftwarePraktikum/src/main/java/controller/GreetingController.java
@@ -1,17 +1,25 @@
 package controller;
 
+import learning.Benutzer;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/")
 public class GreetingController {
-	@RequestMapping(method=RequestMethod.GET)
-	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
+	
+	@RequestMapping(value = "/", method=RequestMethod.GET)
+	public String greeting( Model model) {
+		model.addAttribute("benutzer", new Benutzer());
 		return "index";
+	}
+	
+	@RequestMapping(value= "/medium", method = RequestMethod.POST)
+	public String login(@ModelAttribute Benutzer benutzer, Model model){
+//		benutzer.login())
 	}
 }
