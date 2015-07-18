63348ba02a6b828ebe54a3e9cec8bfe2eea23501
Hacki93 <michael_stefan@hotmail.de>
2015-07-18 17:21:52 +0200

    Eigenes Profil, eigene Gruppenliste
    Eigenes Profil:
    - Benutzerinformationen ändern (müsste eigentlich funktionieren geht
    aber nicht in die Controller Methode rein)
    
    eigene Gruppenliste:
    - Alle eigenen Gruppen werden aus der Datenbank ausgelesen und angezeigt
    - Neue Gruppe erstellen (noch Probleme mit der Fachrichtung, sonst
    klappt alles)

--- SoftwarePraktikum/src/main/java/controller/LenasController.java
+++ SoftwarePraktikum/src/main/java/controller/LenasController.java
@@ -1,143 +1,143 @@
-package controller;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-
-import learning.Benutzer;
-import learning.Frage;
-import learning.Gruppe;
-import learning.Inhalt;
-import learning.Quest;
-import learning.Thema;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import datenhaltung.Datenbank;
-
-@Controller
-public class LenasController {
-	Datenbank db;
-	Benutzer angemeldeterBenutzer;
-	Benutzer profilBenutzer;
-	Gruppe gruppe;
-	Quest quest;
-	
-	@RequestMapping(value = "/", method=RequestMethod.GET)
-	public String greeting(Model model) {
-	    db = new Datenbank();
-	    model.addAttribute("benutzer", new Benutzer());
-		return "Startseite";
-	}
-	
-	@RequestMapping(value = "/GruppenProfil/{gruppe_id}", method =RequestMethod.GET)
-	public String gruppenProfilAnzeigen(@PathVariable("gruppe_id") int gruppe_id, Model model){
-		Gruppe gruppe = new Gruppe();
-		for(Object obj : db.tabelleAusgeben(gruppe.getClass())){
-			Gruppe g = (Gruppe) obj;
-			if(g.getId() == (gruppe_id)) {
-				gruppe = g;
-			}
-		}
-		this.gruppe = gruppe;
-		model.addAttribute("frage", new Frage());
-	    model.addAttribute("thema", new Thema());
-	    model.addAttribute("gruppe", gruppe);
-		return "GruppenProfil";
-	}
-	
-	@RequestMapping(value = "/frageErst", method=RequestMethod.POST)
-	public String frageErstellen(@ModelAttribute Frage frage, Model model){
-		Frage neueFrage = new Frage();
-		db.eintragHinzufuegen(neueFrage.getClass(), neueFrage);
-		neueFrage.setText(frage.getText());
-		neueFrage.setBenutzer(angemeldeterBenutzer);
-		System.out.println(gruppe.getName());
-		for (int i=0; i< frage.getZwischenSpeicherAntworten().size(); i++){
-			neueFrage.addAntwortmoeglichkeiten(frage.getZwischenSpeicherAntworten().get(i));
-		}
-		
-		if(frage.isZwischenSpeicherLoesung1()){
-			if (frage.getZwischenSpeicherAntworten().size() > 0){
-			neueFrage.addLoesung(frage.getZwischenSpeicherAntworten().get(0));	
-			}
-		}
-		
-		if(frage.isZwischenSpeicherLoesung2()){
-			if (frage.getZwischenSpeicherAntworten().size() > 1){
-				neueFrage.addLoesung(frage.getZwischenSpeicherAntworten().get(1));	
-			}
-		}
-		
-		if(frage.isZwischenSpeicherLoesung3()){
-			if (frage.getZwischenSpeicherAntworten().size() > 2){
-				neueFrage.addLoesung(frage.getZwischenSpeicherAntworten().get(2));	
-				}
-		}
-		
-		if(frage.isZwischenSpeicherLoesung4()){
-			if (frage.getZwischenSpeicherAntworten().size() > 3){
-				neueFrage.addLoesung(frage.getZwischenSpeicherAntworten().get(3));	
-				}
-		}
-		
-		neueFrage.medium = frage.getMedium();
-		db.eintragAktualisieren(neueFrage.getClass(), neueFrage);
-		gruppe.getFragenpool().addFrage(neueFrage);
-		//Datenbank wird aktualisiert
-		db.eintragAktualisieren(gruppe.getFragenpool().getClass(), gruppe.getFragenpool());
-		db.eintragAktualisieren(gruppe.getClass(), gruppe);
-		
-		
-		for (String s:neueFrage.getAntwortmoeglichkeiten()){
-			System.out.println("Antwort:");
-			System.out.println(s);
-		
-		}
-		for (String s:neueFrage.getLoesung()){
-			System.out.println("Lösung: ");
-			System.out.println(s);
-		}
-		model.addAttribute("frage", new Frage());
-		model.addAttribute("thema", new Thema());
-	    model.addAttribute("gruppe", gruppe);
-		return "GruppenProfil";
-	}
-	
-	@RequestMapping(value = "/QuestStarten")
-	public String questStarten(Model model){
-		Quest quest = new Quest(); 
-		quest =  gruppe.questAntreten(angemeldeterBenutzer);
-		this.quest = quest;
-		Set<Frage> fragen = new HashSet<Frage>();
-		fragen = quest.getFragen();
-		model.addAttribute("fragen", fragen);
-		model.addAttribute("frage1", new Frage());
-		return "Quest";
-	}
-	
-	@RequestMapping(value = "/themaErst", method=RequestMethod.POST)
-	public String themaErstellen(@ModelAttribute Thema thema, Model model){
-		System.out.println("Thema erstellt");
-		System.out.println(thema.getTitel());
-		model.addAttribute("frage", new Frage());
-	    model.addAttribute("thema", new Thema());
-		return "GruppenProfil";
-	}
-	
-	@RequestMapping(value = "/questBeenden")
-	public String questBeenden(@ModelAttribute Frage frage1, Model model){
-		System.out.println("questBeenden-Methode");
-		model.addAttribute("frage", new Frage());
-		model.addAttribute("thema", new Thema());
-	    model.addAttribute("gruppe", gruppe);
-	    System.out.println("jetzt hier");
-	    return "GruppenProfil";
-	}
-	
-}
+//package controller;
+//
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import learning.Benutzer;
+//import learning.Frage;
+//import learning.Gruppe;
+//import learning.Inhalt;
+//import learning.Quest;
+//import learning.Thema;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//import datenhaltung.Datenbank;
+//
+//@Controller
+//public class LenasController {
+//	Datenbank db;
+//	Benutzer angemeldeterBenutzer;
+//	Benutzer profilBenutzer;
+//	Gruppe gruppe;
+//	Quest quest;
+//	
+//	@RequestMapping(value = "/", method=RequestMethod.GET)
+//	public String greeting(Model model) {
+//	    db = new Datenbank();
+//	    model.addAttribute("benutzer", new Benutzer());
+//		return "Startseite";
+//	}
+//	
+//	@RequestMapping(value = "/GruppenProfil/{gruppe_id}", method =RequestMethod.GET)
+//	public String gruppenProfilAnzeigen(@PathVariable("gruppe_id") int gruppe_id, Model model){
+//		Gruppe gruppe = new Gruppe();
+//		for(Object obj : db.tabelleAusgeben(gruppe.getClass())){
+//			Gruppe g = (Gruppe) obj;
+//			if(g.getId() == (gruppe_id)) {
+//				gruppe = g;
+//			}
+//		}
+//		this.gruppe = gruppe;
+//		model.addAttribute("frage", new Frage());
+//	    model.addAttribute("thema", new Thema());
+//	    model.addAttribute("gruppe", gruppe);
+//		return "GruppenProfil";
+//	}
+//	
+//	@RequestMapping(value = "/frageErst", method=RequestMethod.POST)
+//	public String frageErstellen(@ModelAttribute Frage frage, Model model){
+//		Frage neueFrage = new Frage();
+//		db.eintragHinzufuegen(neueFrage.getClass(), neueFrage);
+//		neueFrage.setText(frage.getText());
+//		neueFrage.setBenutzer(angemeldeterBenutzer);
+//		System.out.println(gruppe.getName());
+//		for (int i=0; i< frage.getZwischenSpeicherAntworten().size(); i++){
+//			neueFrage.addAntwortmoeglichkeiten(frage.getZwischenSpeicherAntworten().get(i));
+//		}
+//		
+//		if(frage.isZwischenSpeicherLoesung1()){
+//			if (frage.getZwischenSpeicherAntworten().size() > 0){
+//			neueFrage.addLoesung(frage.getZwischenSpeicherAntworten().get(0));	
+//			}
+//		}
+//		
+//		if(frage.isZwischenSpeicherLoesung2()){
+//			if (frage.getZwischenSpeicherAntworten().size() > 1){
+//				neueFrage.addLoesung(frage.getZwischenSpeicherAntworten().get(1));	
+//			}
+//		}
+//		
+//		if(frage.isZwischenSpeicherLoesung3()){
+//			if (frage.getZwischenSpeicherAntworten().size() > 2){
+//				neueFrage.addLoesung(frage.getZwischenSpeicherAntworten().get(2));	
+//				}
+//		}
+//		
+//		if(frage.isZwischenSpeicherLoesung4()){
+//			if (frage.getZwischenSpeicherAntworten().size() > 3){
+//				neueFrage.addLoesung(frage.getZwischenSpeicherAntworten().get(3));	
+//				}
+//		}
+//		
+//		neueFrage.medium = frage.getMedium();
+//		db.eintragAktualisieren(neueFrage.getClass(), neueFrage);
+//		gruppe.getFragenpool().addFrage(neueFrage);
+//		//Datenbank wird aktualisiert
+//		db.eintragAktualisieren(gruppe.getFragenpool().getClass(), gruppe.getFragenpool());
+//		db.eintragAktualisieren(gruppe.getClass(), gruppe);
+//		
+//		
+//		for (String s:neueFrage.getAntwortmoeglichkeiten()){
+//			System.out.println("Antwort:");
+//			System.out.println(s);
+//		
+//		}
+//		for (String s:neueFrage.getLoesung()){
+//			System.out.println("Lösung: ");
+//			System.out.println(s);
+//		}
+//		model.addAttribute("frage", new Frage());
+//		model.addAttribute("thema", new Thema());
+//	    model.addAttribute("gruppe", gruppe);
+//		return "GruppenProfil";
+//	}
+//	
+//	@RequestMapping(value = "/QuestStarten")
+//	public String questStarten(Model model){
+//		Quest quest = new Quest(); 
+//		quest =  gruppe.questAntreten(angemeldeterBenutzer);
+//		this.quest = quest;
+//		Set<Frage> fragen = new HashSet<Frage>();
+//		fragen = quest.getFragen();
+//		model.addAttribute("fragen", fragen);
+//		model.addAttribute("frage1", new Frage());
+//		return "Quest";
+//	}
+//	
+//	@RequestMapping(value = "/themaErst", method=RequestMethod.POST)
+//	public String themaErstellen(@ModelAttribute Thema thema, Model model){
+//		System.out.println("Thema erstellt");
+//		System.out.println(thema.getTitel());
+//		model.addAttribute("frage", new Frage());
+//	    model.addAttribute("thema", new Thema());
+//		return "GruppenProfil";
+//	}
+//	
+//	@RequestMapping(value = "/questBeenden")
+//	public String questBeenden(@ModelAttribute Frage frage1, Model model){
+//		System.out.println("questBeenden-Methode");
+//		model.addAttribute("frage", new Frage());
+//		model.addAttribute("thema", new Thema());
+//	    model.addAttribute("gruppe", gruppe);
+//	    System.out.println("jetzt hier");
+//	    return "GruppenProfil";
+//	}
+//	
+//}
